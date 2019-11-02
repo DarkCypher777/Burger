66a5df6f01bcad4ac0024dbb680b9a9438ce298b
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-11-02 18:22:22 -0400

    Alpha files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Epic Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Crabby Patty", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Monster Patty", true);
